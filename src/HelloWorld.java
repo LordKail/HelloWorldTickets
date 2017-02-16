f0a76606758ebc94628d5a5f9036c9c6e089fcba
LordKail <Al3xleet@hotmail.com>
2017-02-16 13:35:27 +0000

    Adding the first versions of the files.

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+/**
+ * Sample Hello World application.
+ */
+
+public class HelloWorld {
+
+    /*
+    @tckt Extend the Hello World application to take user input.
+    @type TASK
+    @priority 5
+     */
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
---
+++ src/WordCount.java
@@ -0,0 +1,13 @@
+/**
+ * Sample WordCount application.
+ */
+
+public class WordCount {
+
+    /*
+    @tckt Create a word count program.
+    @type TASK
+    @priority 9
+     */
+    
+}
